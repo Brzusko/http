8b5e8b277ab3e7011571fa97dfc0b866e0b7572e
w.brzuszkiewicz <wojciech.brzuszkiewicz@hotmail.com>
2021-04-16 13:39:42 +0200

    init

---
+++ engine/function/itemparser/itemlistparser.php
@@ -0,0 +1,34 @@
+<?php 
+
+/* Returns a PHP array $id => 'name'
+	 $items = getItemList();
+	 echo $items[2160]; // Returns 'Crystal Coin'
+*/
+
+function getItemList() {	
+	return parseItems();
+}
+
+function getItemById($id) {
+	$items = parseItems();
+	if(isset($items[$id])) {
+		return $items[$id];
+	}
+	return false;
+}
+
+function parseItems() {
+	global $config;
+
+	$items = simplexml_load_file($config['server_path'].'/data/items/items.xml');
+	$out = array();
+	
+	// Create our parsed item list
+	foreach ($items->children() as $item) {
+		if ($item['id'] && $item['name'] != NULL) {
+			$out[(int)$item['id']] = (string)$item['name'];
+		}
+	}
+
+	return $out;
+}
